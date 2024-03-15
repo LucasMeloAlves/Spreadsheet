c6e9b3ab2d6e935980808d384d6bba81c17a4992
lucas <lucas.melo@edu.unifi.it>
2024-03-15 18:10:50 +0100

    prova

--- Spreadsheet.h
+++ Spreadsheet.h
@@ -4,5 +4,77 @@
 
 #ifndef SPREADSHEET_SPREADSHEET_H
 #define SPREADSHEET_SPREADSHEET_H
+#include <QApplication>
+#include <QTableWidget>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QMessageBox>
+#include <QDebug>
+
+#include <QApplication>
+#include <QTableWidget>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QMessageBox>
+#include <QDebug>
+
+class Spreadsheet : public QWidget {
+public:
+    Spreadsheet(QWidget *parent = nullptr) : QWidget(parent) {
+        setupUi();
+    }
+
+private:
+    void setupUi() {
+
+        QVBoxLayout *layout = new QVBoxLayout(this);
+
+
+        table = new QTableWidget(15, 26, this);
+        layout->addWidget(table);
+
+
+        operationLineEdit = new QLineEdit(this);
+        layout->addWidget(operationLineEdit);
+
+
+        QPushButton *calculateButton = new QPushButton("Calcola", this);
+        connect(calculateButton, &QPushButton::clicked, this, &Spreadsheet::calculate);
+        layout->addWidget(calculateButton);
+
+        setLayout(layout);
+    }
+
+    void calculate() {
+        QString operation = operationLineEdit->text();
+
+        if (operation.isEmpty()) {
+            QMessageBox::warning(this, "Errore", "Inserisci un'espressione nell'operazione");
+            return;
+        }
+
+        // Esegui il calcolo dell'espressione
+        try {
+            QVariant result = calculateExpression(operation);
+            if (result.isValid()) {
+                QMessageBox::information(this, "Risultato", "Il risultato è: " + result.toString());
+            } else {
+                QMessageBox::warning(this, "Errore", "Impossibile valutare l'operazione");
+            }
+        } catch (const std::exception &e) {
+            QMessageBox::warning(this, "Errore", e.what());
+        }
+    }
+
+
+    QTableWidget *table;
+    QLineEdit *operationLineEdit;
+};
+
+
 
 #endif //SPREADSHEET_SPREADSHEET_H
--- main.cpp
+++ main.cpp
@@ -1,36 +1,12 @@
 #include <QApplication>
-#include <QTableWidget>
-#include <QTableWidgetItem>
-#include <QVBoxLayout>
-#include <QWidget>
+#include "Spreadsheet.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    // Creazione di una tabella con 5 righe e 3 colonne
-    QTableWidget table(20, 30);
-
-    // Inserimento di intestazioni di riga e colonna
-
-
-    // Inserimento dati nella tabella
-    QTableWidgetItem *item1 = new QTableWidgetItem("Marco");
-    table.setItem(0, 0, item1);
-
-    QTableWidgetItem *item2 = new QTableWidgetItem("Rossi");
-    table.setItem(0, 1, item2);
-
-    QTableWidgetItem *item3 = new QTableWidgetItem("25");
-    table.setItem(0, 2, item3);
-
-    // Aggiunta della tabella a un layout
-    QWidget window;
-    QVBoxLayout *layout = new QVBoxLayout(&window);
-    layout->addWidget(&table);
-
-    window.setLayout(layout);
-    window.setWindowTitle("Foglio di calcolo con Qt");
-    window.show();
+    Spreadsheet spreadsheet;
+    spreadsheet.setWindowTitle("Foglio di Calcolo Qt");
+    spreadsheet.show();
 
     return app.exec();
 }
